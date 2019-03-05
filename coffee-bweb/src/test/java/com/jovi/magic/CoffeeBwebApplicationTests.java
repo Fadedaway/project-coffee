3b22472a2a340424e7fd20f15cede30c9867445e
fanjiawei <fanjiawei@kedacom.com>
2019-03-05 18:56:35 +0800

    init project

---
+++ coffee-bweb/src/test/java/com/jovi/magic/CoffeeBwebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jovi.magic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoffeeBwebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
