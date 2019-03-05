526fb87e43d04d1fb96bd8176a96ce6ddd4834f4
fanjiawei <fanjiawei@kedacom.com>
2019-03-05 15:41:37 +0800

    init project

---
+++ src/main/java/com/jovi/magic/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.jovi.magic.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fanjiawei
+ * @date Created on 2019/3/5
+ */
+@RestController
+public class UserController {
+}
